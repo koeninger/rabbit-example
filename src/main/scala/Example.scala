ff058997bc79dd3f331b99a24a8514273a9b9aac
koeninger <ckoeninger@digby.com>
2013-09-25 09:09:26 -0500

    report on shutdown

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -37,13 +37,24 @@ object Consumer extends App {
   val t1 = System.currentTimeMillis
   val count = new java.util.concurrent.atomic.AtomicInteger(0)
   val (conn, channel) = Boilerplate.connect
+ 
+  def report = println("%s messages in %s millis".format(count, System.currentTimeMillis - t1))
+  
+  Runtime.getRuntime.addShutdownHook(new Thread {
+    override def run {
+      report
+      channel.close
+      conn.close
+    }
+  })
+
   channel.basicConsume("hello-queue", false, "hello-consumer",
     new DefaultConsumer(channel) {
       override def handleDelivery(consumerTag: String, envelope: Envelope, props: AMQP.BasicProperties, body: Array[Byte]) {
         val deliveryTag = envelope.getDeliveryTag
         val b = new String(body)
         if (b == "quit") {
-          println("%s messages in %s millis".format(count, System.currentTimeMillis - t1))
+          report
           channel.basicAck(deliveryTag, false)
         } else {
           count.incrementAndGet
